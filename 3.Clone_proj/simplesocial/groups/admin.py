7a7da333738b4f832cf2ffa2f95db22fe4590517
Bojidar Boradjiev -X (bboradji - ONPROCESS TECHNOLOGY INC at Cisco) <bboradji@cisco.com>
2020-05-08 13:27:29 +0300

    Initial Commit

---
+++ 3.Clone_proj/simplesocial/groups/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
